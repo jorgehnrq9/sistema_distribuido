4cd83f9db3c3562970589beb2c8bc263966baaaa
jorgehnrq9 <jorgehnrq9@gmail.com>
2017-05-21 19:43:44 -0300

    Add files via upload

--- cadastro.py
+++ cadastro.py
@@ -1,11 +1,30 @@
 listaproduto = []
 def exibirMenu():
+    print("0 - Atualizar produto")
     print("1 - Cadastrar Produto")
     print("2 - Excluir um produto")
     print("3 - Listar produto")
     print("4 - Sair")
+    print("\n")
     opcao = int(input("Digite uma opcao: "))
     return opcao
+
+def atualizarproduto():
+    nomeproduto = input("Digite o nome do produto a ser atualizado: ")
+    indice = -1
+    encontrou = False
+    for elemento in listaproduto:
+        indice += 1
+        if elemento == nomeproduto:
+            encontrou = True
+            
+        if (encontrou):
+            del listaproduto[indice]
+            novoproduto = input("Digite o novo nome ao produto: ")
+            listaproduto.append(novoproduto)
+            break
+            
+
 
 def inserirproduto():
     novoproduto = input("Digite o nome do produto: ")
@@ -38,4 +57,6 @@ while True:
         inserirproduto()
     elif opcao == 2:
         excluirproduto()
+    elif opcao == 0:
+        atualizarproduto()
         print('\n')
