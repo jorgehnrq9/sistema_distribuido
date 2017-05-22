b3a76f875c6e01c3a796eca40e1dea26e2d2f7a1
jorgehnrq9 <jorgehnrq9@gmail.com>
2017-05-21 22:30:36 -0300

    atualização do codigo

--- cliente.py
+++ cliente.py
@@ -18,7 +18,7 @@ class Send:
   return self.new
  
 #função esperar - Thread
-def esperar(tcp,send,host='192.168.0.2',port=5000):
+def esperar(tcp,send,host='127.0.0.1',port=5000):
  destino=(host,port)
  #conecta a um servidor
  tcp.connect(destino)
@@ -37,8 +37,8 @@ if __name__ == '__main__':
  print('Digite IP do servidor(localhost): ')
  host=input()
   
- if host=='192.168.0.2':
-  host = '192.168.0.2'
+ if host=='127.0.0.1':
+  host = '127.0.0.1'
   
  #cria um socket
  tcp=socket(AF_INET,SOCK_STREAM)
